1fe66d468fc31344a0797c036eabbe7c4a7bb87b
quasimodeaux <gatchey@yahoo.com>
2017-06-07 14:27:46 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,9 +6,11 @@ $( document ).ready(function() {
 $("#add-food").on("click", function() {
 
 	    event.preventDefault();
+      console.clear()
 
 	var value = $('#foody_input').val();
 	console.log(value);
+
 
 	   var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=601&entity_type=city&apikey=c3268e89927f3e51654afc5f7b0c4f4b&count=5&q=" +value;        // =================================================================
         $.ajax({
