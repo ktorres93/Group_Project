d7d00a1bd897128b5dbd8180c2237cb843fe2ef6
quasimodeaux <gatchey@yahoo.com>
2017-06-07 14:32:52 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,15 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-//do this on click
+// do this on click
 
 $("#add-food").on("click", function() {
+      event.preventDefault();
+          console.clear();
+      var value = $('#foody_input').val();
+      var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=601&entity_type=city&apikey=c3268e89927f3e51654afc5f7b0c4f4b&count=5&q=" + value;
+      console.log(value);
 
-	    event.preventDefault();
-      console.clear()
-
-	var value = $('#foody_input').val();
-	console.log(value);
-
-
-	   var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=601&entity_type=city&apikey=c3268e89927f3e51654afc5f7b0c4f4b&count=5&q=" +value;        // =================================================================
         $.ajax({
           url: queryURL
         }).done(function  (response){
@@ -24,17 +21,12 @@ $("#add-food").on("click", function() {
           var address = response.restaurants[i].restaurant.location.address;
           var cost = response.restaurants[i].restaurant.average_cost_for_two;
 
-
-
-
           console.log('name:', name);
           console.log('address:', address);
           console.log('cost for two:', cost);
           console.log('latitude:', latitude);
           console.log('longitude:', longitude);
     //      console.log('url:', response.restaurants[i].restaurant.url);
-          
-
         if (response.restaurants[i].restaurant.has_online_delivery == 0){
           console.log("no delivery");
         }
@@ -43,11 +35,8 @@ $("#add-food").on("click", function() {
 
           }
           };
+
         });
-
-
-        
-        // Do this after click 
 
 });
 
