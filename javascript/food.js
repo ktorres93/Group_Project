f719956b89189fadd10ce3d5b811eef70dd163dd
Zubaer I Ahmed <ishugard@gmail.com>
2017-06-12 14:08:42 -0400

    both maps functional

--- javascript/food.js
+++ javascript/food.js
@@ -1,5 +1,9 @@
 var coordinates = [];
 var array = ["this", "this"];
+var names = [];
+var addresses = [];
+var cities = [];
+var prices = [];
 
 $(document).ready(function() {
     console.log("ready!");
@@ -12,17 +16,29 @@ $(document).ready(function() {
         var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id=601&entity_type=city&apikey=c3268e89927f3e51654afc5f7b0c4f4b&count=3&q=" + value;
         console.log(value);
 
+
         $.ajax({
             url: queryURL
         }).done(function(response) {
+             names = [];
+             addresses = [];
+             cities = [];
+             prices = [];
+            console.clear();
+            event.preventDefault();
             console.log(response);
+            $("#well-section").html("");
+            coordinates = [];
+           
             for (var i = 0; i < response.restaurants.length; i++) {
+                // Initializes our variables to utilize the API's properties
                 var latitude = response.restaurants[i].restaurant.location.latitude;
                 var longitude = response.restaurants[i].restaurant.location.longitude;
                 var name = response.restaurants[i].restaurant.name;
                 var address = response.restaurants[i].restaurant.location.address;
                 var locality_verbose = response.restaurants[i].restaurant.location.locality_verbose;
                 var cost = response.restaurants[i].restaurant.average_cost_for_two;
+                // Logs each of the restuarant's information.
                 console.log('name:', name);
                 console.log('address:', address);
                 console.log('city:', locality_verbose);
@@ -30,7 +46,7 @@ $(document).ready(function() {
                 console.log('latitude:', latitude);
                 console.log('longitude:', longitude);
 
-                //      console.log('url:', response.restaurants[i].restaurant.url);
+                //   Logs if the restaurant has delivery but I don't think this works on the API itself. Always get no.
                 if (response.restaurants[i].restaurant.has_online_delivery == 0) {
                     console.log("no delivery");
                 } else if (response.restaurants[i].restaurant.has_online_delivery == 1) {
@@ -38,6 +54,7 @@ $(document).ready(function() {
 
                 }
 
+// Appends the reponse information to the top results div i.e. our article well.
 
                 var wellSection = $("<div>");
 
@@ -46,40 +63,49 @@ $(document).ready(function() {
                 wellSection.empty();
                 $("#well-section").append(wellSection);
 
-                // Confirm that the specific JSON for the article isn't missing any details
-                // If the article has a headline include the headline in the HTML
+
                 if (name !== "null") {
                     $("#article-well-")
                         .append(
-                            "<h6 class='articleHeadline'><span class='label label-primary'>" +
+                            "<h3 class='articleHeadline'><span class='label label-primary'>" +
                             name + "</span><strong> " +
-                            address + "</strong></h6>" );
-                        //    + "<h5 id='lat'>" + latitude + "</h5>" + "<h5 id='long'>" + longitude + "</h5>"
-                        
+                            address + "</strong></h3>" + "<h2 id='lat'>" + latitude + "</h2>" + "<h2 id='long'>" + longitude + "</h2>"
+                        );
 
-                    // Log the first article's headline to console
-                    console.log(name);
+
+                
                 }
+
+// Pushes the response information into their respective arrays
+
                 coordinates.push(latitude, longitude);
+                names.push(name);
+                addresses.push(address);
+                cities.push(locality_verbose);
+
+
             };
+//Calls initialize map function
+
             initMap();
         });
+
+
     });
+
+
 });
 
 
-
-
 function initMap() {
-    for (var i = 0; i < coordinates.length; i++) {
-        console.log(name);
-    }
+
     var uluru = {
         lat: parseInt($("#lat").html()),
         lng: parseInt($("#long").html())
     };
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
+
         center: uluru
     });
     var marker, i;
@@ -94,54 +120,17 @@ function initMap() {
 
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-                infowindow.setContent(name);
+                infowindow.setContent(names[i] + "<br>" + addresses[i] + "<br>" + cities[i] + "<br>" +  coordinates[i] + "<br>" + coordinates[i+1]);
+
                 infowindow.open(map, marker);
             }
         })(marker, i));
     }
 
 
-map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 15
-        });
-        infoWindow = new google.maps.InfoWindow;
-
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-      
-       //   var marker = new google.maps.Marker({
-       //   position: center,
-       //   map: map;
-       //   });
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('you are here');
-            infoWindow.open(map);
-            map.setCenter(pos);
 
 
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
-
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+};
 /*
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB-IcM0mrkbjxEb2g04rQFvnsvZV23YLtU&callback=initMap">
