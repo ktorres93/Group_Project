89eeb87597440590273533bda983a2599b5b4660
Kevin <ktorres93@knights.ucf.edu>
2017-06-13 11:25:46 -0400

    linked all js files

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,14 +2,23 @@
 
 $(document).ready(function(){
 	console.log("ready!");
+ 		$('.carousel').carousel();
+// Next slide
+$('.carousel').carousel('next');
+$('.carousel').carousel('next', 3); // Move next n times.
+// Previous slide
+$('.carousel').carousel('prev');
+$('.carousel').carousel('prev', 4); // Move prev n times.
+// Set to nth slide
+$('.carousel').carousel('set', 4);
 
+
+ 		
 		navigator.geolocation.getCurrentPosition(initialise);
 
-	$("#McDonald").on("click", function() {
+//	$("#McDonald").on("click", function() {
 
-
-
-	});
+//	});
 
 
 });
